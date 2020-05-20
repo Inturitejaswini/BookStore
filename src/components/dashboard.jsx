677f500346a88fbcb0979c380e489ebffd0925f9
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-05-20 13:32:51 +0530

    [Tejaswini].added new file

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -66,7 +66,9 @@ export class Dashboard extends Component {
         }
     }
 
-    
+    changeText(currentText) {
+        this.setState({ currentText });
+    }
     render() {
         return (
             <div id="dashboard-appbar">

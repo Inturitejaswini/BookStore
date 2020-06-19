4cb0be5d53175eeebde47ca033f022c516fe39b5
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-19 13:27:14 +0530

    [Tejaswini].refactor  some changes in reducer

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -41,10 +41,10 @@ class Cart extends Component {
         {cartItems.length === 0 ? (
           <div className="cart cart-header">Cart is empty</div>
         ) : (
-          <div className="cart cart-header">
-            You have {cartItems.length} in the cart{" "}
-          </div>
-        )}
+            <div className="cart cart-header">
+              You have {cartItems.length} in the cart{" "}
+            </div>
+          )}
 
         {order && (
           <Modal isOpen={true} onRequestClose={this.closeModal}>
